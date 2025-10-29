8b1289a7b1e617f3bfb20ff6c878caab30e2b65e
pirat <superpes3000@gmail.com>
2025-10-29 18:48:39 +0500

    add migrations add index controller

--- app/Models/PostTag.php
+++ app/Models/PostTag.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class PostTag extends Model
 {
     use HasFactory;
-
-    protected $table = 'categories';
+    protected $table = 'post_tags';
     protected $guarded = false;
+
 }
